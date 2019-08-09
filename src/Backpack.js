434024a67bfeec2825578091bf9842381eb901b6
Mandela Effah Effiong <FBN@Mandelas-MacBook-Pro.local>
2019-08-09 14:34:07 -0500

    Build Folder

---
+++ src/Backpack.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Backpack extends Component {
+    render() {
+        return (
+            <div>
+                This is the inside of the Backpack
+            </div>
+        )
+    }
+}
+
+export default Backpack
---
+++ src/BackpackItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class BackpackItem extends Component {
+    render() {
+        return (
+            <div>
+                t
+            </div>
+        )
+    }
+}
+
+export default BackpackItem

61a4af03a7d5dea71e79924b2dec628e4f0f8fe4
Mandela Effah Effiong <FBN@Mandelas-MacBook-Pro.local>
2019-08-11 16:07:45 -0500

    School Component

---
+++ build/precache-manifest.e1ec1699ea70e1cbc7e79337059e2b6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f8221177f5be1dd7412e27d6e9655d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad1e17f88deadbaeba5",
+    "url": "/static/css/main.40c78b87.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1ad1e17f88deadbaeba5",
+    "url": "/static/js/main.fd9d806b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

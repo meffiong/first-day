c35ae7733a55308d3f75ed2fd98aba72e565100f
Mandela Effah Effiong <FBN@Mandelas-MacBook-Pro.local>
2019-08-11 16:04:06 -0500

    School Component

---
+++ src/School.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './School.css';
+
+class School extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Are you ready for your first day of school?!</h1>
+            </div>
+        )
+    }
+}
+
+export default School
